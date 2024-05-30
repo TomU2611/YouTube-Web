bc386b810b4c86e966ff91794958700b0913bce0
itayzahor <itayzahor123@gmail.com>
2024-05-30 19:16:02 +0300

    now the real commit

--- my-yt-web/src/App.js
+++ my-yt-web/src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import TopBAr from './components/TopBar';
 import SideBar from './components/SideBar';
 import VideoList from './components/VideoList';
+import {defaultVideos} from './data/videos';
 
 function App() {
   return (
     <div className="App">
-      <TopBAr />
-      <SideBar />
-      <VideoList />
-
+      <VideoList videos={defaultVideos}/>
     </div>
   );
 }
--- my-yt-web/src/videoList/videoItem/VideoItem.js
+++ my-yt-web/src/videoList/videoItem/VideoItem.js
@@ -1,9 +1,8 @@
 function VideoItem({ title, author, image, views, date, onVideoSelect, video }) {
     return (
-        <div onClick={() => onVideoSelect(video)}>
+        <div >
             <h2>{title}</h2>
             <p>Author: {author}</p>
-            <img src={image} alt={title} />
             <p>Views: {views}</p>
             <p>Date: {date}</p>
         </div>
