be7fbf260b82f1e49382cb1ee3a8ea303476596f
itayzahor <itayzahor123@gmail.com>
2024-05-30 20:45:30 +0300

    list of videos commit

--- my-yt-web/src/videoList/Videolist.js
+++ my-yt-web/src/videoList/Videolist.js
@@ -2,7 +2,7 @@ import VideoItem from "./videoItem/VideoItem";
 import videos from "../data/defaultVideos.json";
 import { useState } from 'react';
 
-function Videolist() {
+function VideoList() {
     const [videosList, setVideosList] = useState(videos);
     console.log(videosList)
     var i = 1
