e3dd862e21530108a3c8393431f51565ea9e8bae
itayzahor <itayzahor123@gmail.com>
2024-06-03 17:08:09 +0300

    commit

--- my-yt-web/src/videoList/videoItem/VideoItem.js
+++ my-yt-web/src/videoList/videoItem/VideoItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './VideoItem.css';
-
+//display the video item
 function VideoItem({ title, author, views, timeAgo, photo}) {
     const imagePath = `${process.env.PUBLIC_URL}/photos/${photo}`;
     return (
