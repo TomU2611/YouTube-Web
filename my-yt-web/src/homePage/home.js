7e23d795165894700edcc7bce0c7d446c94a5d55
mark sheinberg <marksheinberg01.gmail.com>
2024-06-03 18:59:31 +0300

    commit3

---
+++ my-yt-web/src/homePage/home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import TopBar from '../topBar/TopBar'; 
+import Dropdown from '../topBar/Dropdown';
+import '../homePage/Home.css';
+import SideBar from '../sideBar/SideBar';
+import VideoList from '../videoList/Videolist';
+
+function Home({ darkMode, toggleDarkMode }) {
+  return (
+    <div className={`Home ${darkMode ? 'dark-mode' : ''}`}>
+      <TopBar />
+      <VideoList />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3">
+            <SideBar />
+          </div>
+          <div className="col-sm-9">
+            <Dropdown toggleDarkMode={toggleDarkMode} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
